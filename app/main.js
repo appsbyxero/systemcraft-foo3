7921a9e7279d633213503aad5808d71d0750c365
appsbyxero <appxero@gmail.com>
2024-08-25 12:07:29 +0530

    Provide starter code

--- app/main.js
+++ app/main.js
@@ -1,3 +1,14 @@
 const process = require("process");
+const fs = require('node:fs');
+const readline = require('readline');
 
-// Write your code here!
+// This passes the first task!
+const file = readline.createInterface({
+    input: fs.createReadStream(process.argv[3]),
+    output: process.stdout,
+    terminal: false
+});
+
+file.on('line', (line) => {
+    console.log(line);
+});
